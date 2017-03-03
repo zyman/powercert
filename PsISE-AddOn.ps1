d8c909358045e87ba0768cb121d7bed2a324e2e6
Martynas Dabasinskas <martynas.dabasinskas@gmail.com>
2017-03-03 15:09:39 +0000

    Fixes

--- PsISE-AddOn.ps1
+++ PsISE-AddOn.ps1
@@ -6,9 +6,9 @@ $menuAdded = $psISE.CurrentPowerShellTab.AddOnsMenu.SubMenus.Add(
     {
         $sig = (Invoke-WebRequest -Uri "http://localhost:50933/api/sign" -Method Post -Body $("=" + $psISE.CurrentFile.Editor.Text) -UseDefaultCredentials).Content;
         $psISE.CurrentFile.Editor.Clear();
-        $psISE.CurrentFile.Editor.InsertText( $sig.Replace("\r\n", "`r`n"))
+        $psISE.CurrentFile.Editor.InsertText( $sig.Replace("\r\n", "`r`n")).Replace("\""", """")
         $psISE.CurrentFile.Editor.Text = $psISE.CurrentFile.Editor.Text.Substring(1,$psISE.CurrentFile.Editor.Text.Length-2)
-        #$psISE.CurrentFile.Editor.InsertText($sig.Replace("\r\n", "`r`n").Replace("\"", """))
+        #$psISE.CurrentFile.Editor.InsertText($sig.Replace("\r\n", "`r`n").Replace("\""", """"))
         
     },
     "Alt+S") 
